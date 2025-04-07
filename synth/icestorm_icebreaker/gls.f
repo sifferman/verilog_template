9ce27de0bee01dad091ec7cbe2a922d62451d4e0
Ethan Sifferman <ethan@sifferman.dev>
2025-04-06 19:20:18 -0700

    added simlib.v to icestorm_icebreaker_gls

--- synth/icestorm_icebreaker/gls.f
+++ synth/icestorm_icebreaker/gls.f
@@ -3,6 +3,7 @@ rtl/config_pkg.sv
 
 -DNO_ICE40_DEFAULT_ASSIGNMENTS
 ${YOSYS_DATDIR}/ice40/cells_sim.v
+${YOSYS_DATDIR}/simlib.v
 
 synth/icestorm_icebreaker/build/synth.v
 synth/icestorm_icebreaker/blinky_runner.sv
